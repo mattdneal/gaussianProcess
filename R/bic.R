84fc8759dae93b8089341b2259b3f4cfc7fff21e
mneal <mattdneal@gmail.com>
2016-04-14 17:07:53 +0100

    Changed bic.model.search to model.search and changed to allow custom score functions.

--- R/bic.R
+++ R/bic.R
@@ -157,6 +157,7 @@ posterior.laplace.approximation <- function(gp.obj) {
 #' @param reset.params By default the search starts with the optimum hyperparameter values found in the previous step in the search (with new hyperparameters fitted from random start points). Setting this to TRUE causes all hyperparameters to be randomly set at each step.
 #' @param max.new.nodes The number of kernel instances to add to the base model.
 #' @param revisit.kernels Whether to revisit previously-assessed kernels when deleting nodes from the current best candidate.
+#' @param scoring.function The scoring function for the GPs in the search. Must take a trained gaussianProcess object as its only parameter, and return a scalar score. If \code{scoring.function} returns a numeric vector only the first entry will be used.
 #' @param ... Additional parameters to be passed to gp.obj$fit.hyperparameters (see \code{\link{gaussianProcess}})
 #'
 #' @return The trained gaussianProcess object with the best BIC.
@@ -169,19 +170,33 @@ posterior.laplace.approximation <- function(gp.obj) {
 #' y <- sin(1/(x^2 + 0.15))
 #' mt <- create.model.tree.builtin()
 #' gp <- bic.model.search(x, y, mt)
-bic.model.search <- function(x,
-                             y,
-                             base.model.tree,
-                             plot.gp=FALSE,
-                             reset.params=FALSE,
-                             max.new.nodes=3,
-                             revisit.kernels=TRUE,
-                             ...) {
-  model.bic.vec <- numeric()
+model.search <- function(x,
+                         y,
+                         base.model.tree,
+                         plot.gp=FALSE,
+                         reset.params=FALSE,
+                         max.new.nodes=3,
+                         revisit.kernels=TRUE,
+                         scoring.function=bayesian.information.criterion,
+                         return.all.models=FALSE,
+                         alternate.scoring.functions=list(),
+                         ...) {
+  model.score.vec <- numeric()
 
   evaluated.models <- character(0)
 
   cache <- create_cache()
+
+  output <- list()
+
+  if (return.all.models) {
+    output$model.list <- list()
+  }
+
+  scoring.function.name <- as.character(substitute(scoring.function))
+
+  scores <- data.frame(matrix(nrow=0, ncol=1 + length(alternate.scoring.functions)))
+  colnames(scores) <- c(scoring.function.name, names(alternate.scoring.functions))
 
   if (nrow(base.model.tree$tree) != 0) {
     # There is a base model defined, so first evaluate that.
@@ -191,35 +206,49 @@ bic.model.search <- function(x,
 
     result <- fit.hyperparams(gp.obj, ...)
     gp.obj <- result$gp
-    model.bic.vec[as.character(gp.obj$kernel$kernel)] <- bayesian.information.criterion(gp.obj)
+    model.name <- as.character(gp.obj$kernel$kernel)
+
+    model.score.vec[model.name] <- scoring.function(gp.obj)
+
+    scores[model.name, scoring.function.name] <- model.score.vec[model.name]
+
+    for (i in names(alternate.scoring.functions)) {
+      scores[model.name, i] <- alternate.scoring.functions[[i]](gp.obj)
+    }
+
+    if (return.all.models) {
+      output$model.list[[model.name]] <- gp.obj
+    }
+
     evaluated.models <- c(evaluated.models, as.character(gp.obj$kernel$kernel))
     print(paste("Base model:", as.character(gp.obj$kernel$kernel)))
-    print(paste("Base model BIC:", model.bic.vec[as.character(gp.obj$kernel$kernel)]))
+    print(paste("Base model Score:", model.score.vec[as.character(gp.obj$kernel$kernel)]))
 
-    best.bic <- model.bic.vec[as.character(gp.obj$kernel$kernel)]
+    best.score <- model.score.vec[as.character(gp.obj$kernel$kernel)]
     best.gp <- gp.obj
     best.model <- gp.obj$kernel$kernel
     if (plot.gp) plot(gp.obj)
+
   } else {
-    best.bic <- Inf
+    best.score <- Inf
     best.gp <- NULL
     best.model <- base.model.tree
   }
 
-  bic.improved <- TRUE
+  score.improved <- TRUE
 
   new.nodes <- 0
 
-  while (bic.improved & new.nodes < max.new.nodes) {
+  while (score.improved & new.nodes < max.new.nodes) {
     new.nodes <- new.nodes + 1
 
-    if (reset.params & best.bic != Inf & any(!is.na(best.model$all.hyper.params))) {
+    if (reset.params & best.score != Inf & any(!is.na(best.model$all.hyper.params))) {
       reset.vec <- c(FALSE, TRUE)
     } else {
       reset.vec <- FALSE
     }
 
-    bic.improved <- FALSE
+    score.improved <- FALSE
     next.models <- generate.next.models(best.model)
 
     if (!revisit.kernels) {
@@ -259,21 +288,36 @@ bic.model.search <- function(x,
         }
 
         if (result$optimx.obj["convcode"] == 0) {
-          model.bic <- bayesian.information.criterion(gp.obj)
-          model.bic.vec[paste(as.character(gp.obj$kernel$kernel), as.character(reset), sep=".")] <- model.bic
+          model.score <- scoring.function(gp.obj)
+
+          model.name <- paste(as.character(gp.obj$kernel$kernel), as.character(reset), sep=".")
+
+          model.score.vec[model.name] <- model.score
+
+          scores[model.name, scoring.function.name] <- model.score.vec[model.name]
+
+          for (i in names(alternate.scoring.functions)) {
+            scores[model.name, i] <- alternate.scoring.functions[[i]](gp.obj)
+          }
+
+          if (return.all.models) {
+            output$model.list[[model.name]] <- gp.obj
+          }
+
           evaluated.models <- c(evaluated.models, as.character(gp.obj$kernel$kernel))
           print(paste("Model:", as.character(gp.obj$kernel$kernel)))
-          print(paste("Model BIC:", model.bic))
+          print(paste("Model Score:", model.score))
           print("Model hyperparams:")
           print(gp.obj$optimized.hyperparams)
           print(paste("sigma_n:", gp.obj$optimized.sigma.n))
-          print(as.matrix(model.bic.vec[order(model.bic.vec)]))
+          print(as.matrix(model.score.vec[order(model.score.vec)]))
+          print(as.matrix(scores[order(model.score.vec), ]))
           if (plot.gp) plot(gp.obj, main=as.character(gp.obj$kernel$kernel))
-          if (model.bic < best.bic) {
-            best.bic <- model.bic
+          if (model.score < best.score) {
+            best.score <- model.score
             best.gp <- gp.obj
             best.model <- gp.obj$kernel$kernel
-            bic.improved <- TRUE
+            score.improved <- TRUE
           }
         } else {
           warnmess <- paste("Model", as.character(current.model), "failed to converge")
@@ -283,5 +327,7 @@ bic.model.search <- function(x,
       }
     }
   }
-  return(best.gp)
+  output$best.gp <- best.gp
+  output$scores <- scores
+  return(output)
 }
